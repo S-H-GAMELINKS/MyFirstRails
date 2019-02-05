af188b53aed73b9c8db78a82fc70a800e76d0c22
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-05 11:04:09 +0900

    add comments  resources #6

--- bbs/config/routes.rb
+++ bbs/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'posts#index'
-  resources :posts
+  resources :posts do
+    resources :comments, :only => [:create, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
