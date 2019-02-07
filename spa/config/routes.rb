c1732f108c92c7ccb14d9d0f0477816b2c09eeea
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-07 12:36:12 +0900

    fix route #14

--- spa/config/routes.rb
+++ spa/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'web#index'
-  get 'web/index'
+  get '/about', to: 'web#index'
+  get '/contact', to: 'web#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
