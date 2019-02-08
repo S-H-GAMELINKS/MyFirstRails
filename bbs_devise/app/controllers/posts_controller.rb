25f8e1aea359af83c6f13bfab529db0d10bc4d9f
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-08 11:52:41 +0900

    add current_user.id save #18

--- bbs_devise/app/controllers/posts_controller.rb
+++ bbs_devise/app/controllers/posts_controller.rb
@@ -28,6 +28,7 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = Post.new(post_params)
+    @post.user_id = current_user.id
 
     respond_to do |format|
       if @post.save
