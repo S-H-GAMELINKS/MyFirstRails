40c69695aec4891308d306a84c355cc1994ff3d4
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-08 11:46:46 +0900

    Migrate DB #18

--- bbs_devise/db/schema.rb
+++ bbs_devise/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_08_024334) do
+ActiveRecord::Schema.define(version: 2019_02_08_024503) do
 
   create_table "comments", force: :cascade do |t|
     t.string "auther"
@@ -18,7 +18,9 @@ ActiveRecord::Schema.define(version: 2019_02_08_024334) do
     t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
