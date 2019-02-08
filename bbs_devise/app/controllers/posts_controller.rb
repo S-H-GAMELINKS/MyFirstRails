9dca98557c009fc8e7527fa1955b9110076e6980
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-08 11:28:38 +0900

    using current_user.name for new comments #18

--- bbs_devise/app/controllers/posts_controller.rb
+++ bbs_devise/app/controllers/posts_controller.rb
@@ -10,6 +10,8 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
+    @comment = Comment.new
+    @comment.auther = current_user.name
   end
 
   # GET /posts/new
