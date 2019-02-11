3ce820ae1aedb825ce1c173f988c11da2bf6f598
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-11 15:31:27 +0900

    add omniauth for twitter #24

--- twitter_login/config/initializers/devise.rb
+++ twitter_login/config/initializers/devise.rb
@@ -261,6 +261,8 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
+  config.omniauth :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_SECRET_KEY']
+
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
