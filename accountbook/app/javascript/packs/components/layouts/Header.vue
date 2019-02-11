915b0b55ea7dfc6f16c00149cacc025d22861428
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-11 17:57:18 +0900

    add layouts/Header.vue #26

---
+++ accountbook/app/javascript/packs/components/layouts/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a class="navbar-brand" href="#">Account Book</a>
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Menu
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a href="/" class="dropdown-item">Top</a>
+                    <a href="/about" class="dropdown-item">About</a>
+                    <a href="/contact" class="dropdown-item">Contact</a>
+                </div>
+            </div>
+        </nav>
+    </div>    
+</template>
