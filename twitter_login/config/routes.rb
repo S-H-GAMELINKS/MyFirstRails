b4b9d08c127390f798de2dcc669c4d4ef7c63392
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-11 15:39:31 +0900

    add root path #24

--- twitter_login/config/routes.rb
+++ twitter_login/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'web#index'
   get 'web/index'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
