7b5955a7c6267308c6b63e85105f85d57ba1779d
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-11 17:59:43 +0900

    using Headr.vue #26

--- accountbook/app/javascript/packs/index.js
+++ accountbook/app/javascript/packs/index.js
@@ -2,10 +2,15 @@ import Vue from 'vue/dist/vue.esm';
 import * as Bootstrap from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap';
 
+import Header from './components/layouts/Header.vue';
+
 Vue.use(Bootstrap);
 
 const app = new Vue({
     el: '.app',
+    components: {
+        'nav-bar': Header
+    },
     data: function() {
         return {
             message: "Hello World! For Vue.js & Rails!"
