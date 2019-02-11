62f0db4d190a9065f762a68f7005e2c2dbcffcd8
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-11 15:34:44 +0900

    add find_or_oauth #24

--- twitter_login/app/models/user.rb
+++ twitter_login/app/models/user.rb
@@ -3,4 +3,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable
+
+  def self.find_for_oauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
+      
+    unless user
+      user = User.create(
+        uid:      auth.uid,
+        provider: auth.provider,
+        email:    User.dummy_email(auth),
+        password: Devise.friendly_token[0, 20]
+      )
+      user.save!
+    end
+    current_user = user
+  end
+      
+  private
+      
+    def self.dummy_email(auth)
+      "#{auth.uid}-#{auth.provider}@example.com"
+    end
 end
