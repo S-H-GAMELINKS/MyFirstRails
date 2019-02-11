d86f1b3be5cc38f2315c12fbd2d0734c473bdaf5
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-11 11:24:57 +0900

    first commit #22

---
+++ refinerycms/db/migrate/20190211022422_add_custom_slug_to_refinery_pages.refinery_pages.rb
@@ -0,0 +1,16 @@
+# This migration comes from refinery_pages (originally 20140105190324)
+class AddCustomSlugToRefineryPages < ActiveRecord::Migration[4.2]
+  def up
+    add_column :refinery_pages, :custom_slug, :string unless custom_slug_exists?
+  end
+
+  def down
+    remove_column :refinery_pages, :custom_slug if custom_slug_exists?
+  end
+
+  private
+
+  def custom_slug_exists?
+    column_exists?(:refinery_pages, :custom_slug)
+  end
+end
---
+++ refinerycms/db/migrate/20190211022434_create_seo_meta.seo_meta.rb
@@ -0,0 +1,23 @@
+# This migration comes from seo_meta (originally 20110329222114)
+class CreateSeoMeta < ActiveRecord::Migration[4.2]
+  def self.up
+    create_table :seo_meta do |t|
+      t.integer :seo_meta_id
+      t.string :seo_meta_type
+
+      t.string :browser_title
+      t.string :meta_keywords
+      t.text :meta_description
+
+      t.timestamps :null => false
+    end
+
+    add_index :seo_meta, :id
+    add_index :seo_meta, [:seo_meta_id, :seo_meta_type], :name => :id_type_index_on_seo_meta
+  end
+
+  def self.down
+    drop_table :seo_meta
+  end
+
+end
