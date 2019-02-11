d6b42ed7820bd6b8a1cea04b3db4a64daa2903dd
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-11 11:34:17 +0900

    using locale js in seed.rb #22

--- refinerycms/db/seeds.rb
+++ refinerycms/db/seeds.rb
@@ -6,5 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+I18n.locale = Refinery::I18n.default_locale
+
 # Added by Refinery CMS Pages extension
 Refinery::Pages::Engine.load_seed
