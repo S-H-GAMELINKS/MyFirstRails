6f72f13d159919ea6aef0fdab3d32ab23ecf4757
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-12 12:28:56 +0900

    Migrate DB #26

--- accountbook/db/schema.rb
+++ accountbook/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_11_083520) do
+ActiveRecord::Schema.define(version: 2019_02_12_032831) do
 
   create_table "accounts", force: :cascade do |t|
     t.date "date"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2019_02_11_083520) do
     t.string "category"
     t.text "about"
     t.boolean "income"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
