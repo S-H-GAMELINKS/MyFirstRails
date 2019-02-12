18c740f8bee6192b4951c418d9cfd2c32b7cb186
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-12 12:30:01 +0900

    move api dir #26

--- accountbook/app/controllers/api/categories_controller.rb
+++ accountbook/app/controllers/api/categories_controller.rb
@@ -1,4 +1,4 @@
-class CategoriesController < ApplicationController
+class Api::CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :update, :destroy]
 
   # GET /categories
--- accountbook/config/routes.rb
+++ accountbook/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :categories
   root 'web#index'
   namespace :api, format: 'json' do
     resources :accounts
+    resources :categories
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
