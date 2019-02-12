d13471eb1246e21c9c207993c6b33e3b05dc7eec
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-12 12:14:05 +0900

    add postAccounts method #26

--- accountbook/app/javascript/packs/components/accounts/Index.vue
+++ accountbook/app/javascript/packs/components/accounts/Index.vue
@@ -3,6 +3,11 @@
         <p v-for="(account, key, index) in accounts" :key=index>
             {{account.money}}
         </p>
+        <p>
+            <input v-model="money" class="form-contorl" placeholder="金額を入力してください!">
+            <input v-model="about" class="form-control" placeholder="摘要を入力してください!">
+            <button type="button" class="btn btn-primary" v-on:click="postAccounts">追加</button>
+        </p>
     </div>
 </template>
 
@@ -12,7 +17,9 @@ import axios from 'axios';
 export default {
     data: function() {
         return {
-            accounts: []
+            accounts: [],
+            money: "",
+            about: ""
         }
     },
     mounted: function() {
@@ -26,6 +33,15 @@ export default {
                 }
             }, (error) => {
                 condole.log(error);
+            })
+        },
+        postAccounts: function() {
+            axios.post('/api/accounts', {account: {money: Number(this.money), about: this.about}}).then((response) => {
+                this.accounts.unshift(response.data);
+                this.money = "";
+                this.about = "";
+            }, (error) => {
+                console.log(error);
             })
         }
     }
