a27df718d8d8c344d2fcd27abb5798f14d8330c4
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-12 11:38:11 +0900

    using bootstrap-umi for webpacker dsl #26

--- accountbook/config/webpack/environment.js
+++ accountbook/config/webpack/environment.js
@@ -2,4 +2,13 @@ const { environment } = require('@rails/webpacker')
 const vue =  require('./loaders/vue')
 
 environment.loaders.append('vue', vue)
+
+environment.loaders.append('css', {
+    test: /\.css$/,
+    use: [
+        'style-loader',
+        'css-loader'
+    ]
+})
+
 module.exports = environment
