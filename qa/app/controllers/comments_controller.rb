69f6e4d4aaa2b709bc74c122ea566f66cd0b1424
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-13 11:14:32 +0900

    editing comments #28

--- qa/app/controllers/comments_controller.rb
+++ qa/app/controllers/comments_controller.rb
@@ -1,8 +1,17 @@
 class CommentsController < ApplicationController
     before_action :set_question
+    before_action :set_comment, only: [:edit, :update]
+
+    def edit
+    end
 
     def create
         @question.comments.create! comments_params
+        redirect_to @question
+    end
+
+    def update
+        @comment.update(comments_params)
         redirect_to @question
     end
 
@@ -16,7 +25,11 @@ class CommentsController < ApplicationController
             @question = Question.find(params[:question_id])
         end
 
-         def comments_params
+        def set_comment
+            @comment = Comment.find(params[:id])
+        end
+
+        def comments_params
             params.required(:comment).permit(:content)
         end
 end
--- qa/config/routes.rb
+++ qa/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'questions#index'
   resources :questions do
-    resources :comments, :only => [:edi, :create, :update, :destroy]
+    resources :comments, :only => [:edit, :create, :update, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
