f5d90aed546faae7b29a3d92b6ebfd8fa7393917
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-13 12:40:23 +0900

    Migrate DB #28

--- qa/db/schema.rb
+++ qa/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_13_033852) do
+ActiveRecord::Schema.define(version: 2019_02_13_033950) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -24,7 +24,9 @@ ActiveRecord::Schema.define(version: 2019_02_13_033852) do
     t.integer "question_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["question_id"], name: "index_comments_on_question_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "questions", force: :cascade do |t|
