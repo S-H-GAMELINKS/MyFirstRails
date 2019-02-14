3ebbdf01f664b94e16f90b321d0a3bd777688f4c
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-14 11:33:34 +0900

    check user login & redirect to root #28

--- qa/app/controllers/categories_controller.rb
+++ qa/app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
+  before_action :check_login, only: [:new, :edit, :create, :update, :destroy]
 
   # GET /categories
   # GET /categories.json
@@ -65,6 +66,10 @@ class CategoriesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_category
       @category = Category.find(params[:id])
+    end
+
+    def check_login
+      redirect_to :root if current_user == nil
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
