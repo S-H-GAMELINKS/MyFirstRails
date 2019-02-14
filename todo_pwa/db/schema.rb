ca0cf6d957cdfb9cd0041afac81b1f0021f4bb64
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-14 16:57:01 +0900

    Migrate DB #33

--- todo_pwa/db/schema.rb
+++ todo_pwa/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_14_075543) do
+ActiveRecord::Schema.define(version: 2019_02_14_075643) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_02_14_075543) do
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
 end
