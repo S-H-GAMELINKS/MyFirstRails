fc1a93a917c87abb33206a9b363efe9ad9144149
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-14 16:55:59 +0900

    Scaffold Category #33

--- todo_pwa/config/routes.rb
+++ todo_pwa/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :categories
   root 'tasks#index'
   resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
