c159bd8aa49b31a205f898d903fb2a1727a75623
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-15 16:11:12 +0900

    add params to project_params #32

--- coccoon/app/controllers/projects_controller.rb
+++ coccoon/app/controllers/projects_controller.rb
@@ -69,6 +69,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:title)
+      params.require(:project).permit(:title, tasks_attributes: [:id, :description, :done, :_destroy])
     end
 end
