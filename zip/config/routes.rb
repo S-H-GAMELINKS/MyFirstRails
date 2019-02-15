a5d02c6f535da8e7b69fff8478f2aa0ee40266be
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-15 11:28:25 +0900

    fix root path #30

--- zip/config/routes.rb
+++ zip/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'web#index'
+  root 'zipfiles#index'
   get 'web/index'
   resources :zipfiles, :only => [:index, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
