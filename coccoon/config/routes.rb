d9cec3b1335fa9c301581b560c10644dbeb6bcd8
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-15 20:03:27 +0900

    add root path #32

--- coccoon/config/routes.rb
+++ coccoon/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'projects#index'
   resources :projects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
