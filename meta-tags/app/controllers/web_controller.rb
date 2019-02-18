5a5bf9d16cd42d884d7d56d87b1d30d367d9035d
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-18 13:57:39 +0900

    Create Web index about contact #57

---
+++ meta-tags/app/controllers/web_controller.rb
@@ -0,0 +1,10 @@
+class WebController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
--- meta-tags/config/routes.rb
+++ meta-tags/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'web/index'
+  get 'web/about'
+  get 'web/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
