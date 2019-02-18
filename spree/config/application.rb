c89358d3a20ec039e6e2e1492cb418c7307fd385
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-18 09:35:27 +0900

    using ja.yml for spree

--- spree/config/application.rb
+++ spree/config/application.rb
@@ -23,6 +23,9 @@ module Spree
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Using japanease
+    config.i18n.default_locale = :ja 
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
