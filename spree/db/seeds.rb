b04b13a22d26acb445a44b40fbc9fcfad96ca00e
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-18 09:13:08 +0900

    rails g spree:install --user_class=Spree::User

--- spree/db/seeds.rb
+++ spree/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Spree::Core::Engine.load_seed if defined?(Spree::Core)
+Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
