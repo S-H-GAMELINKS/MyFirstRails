d8e0b9786ab61a22208f6dbb4c3e3b117496c075
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-19 13:56:09 +0900

    add root path #53

--- novel_post/config/routes.rb
+++ novel_post/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'novels#index'
   resources :novels
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
