c05c909680d3b7433999bfb35898fca354ab33f1
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-19 17:29:00 +0900

    add users_controller.rb #53

---
+++ novel_post/app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update]
+
+    # GET /users/1
+    # GET /users/1.json
+    def show
+    end
+  
+    # GET /users/1/edit
+    def edit
+    end
+  
+    # PATCH/PUT /users/1
+    # PATCH/PUT /users/1.json
+    def update
+      respond_to do |format|
+        if @user.update(user_params)
+          format.html { redirect_to @user, notice: 'User name was successfully updated.' }
+          format.json { render :show, status: :ok, location: @user }
+        else
+          format.html { render :edit }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+  
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_user
+        @user = User.find(params[:id])
+      end
+  
+      # Never trust parameters from the scary internet, only allow the white list through.
+      def user_params
+        params.require(:user).permit(:name)
+      end
+  end
