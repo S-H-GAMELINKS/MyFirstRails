0f95c40c49b3882ee2a0dc1ad79c67ee133d1a5a
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-19 17:21:45 +0900

    Migrate DB #53

--- novel_post/db/schema.rb
+++ novel_post/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_081557) do
+ActiveRecord::Schema.define(version: 2019_02_19_082123) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2019_02_19_081557) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_novels_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
