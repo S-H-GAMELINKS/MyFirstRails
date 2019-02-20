7ae6bcf60db77193d820f74f7a15c749766b7dad
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-20 13:50:10 +0900

    Migrate DB #54

--- illust_post/db/schema.rb
+++ illust_post/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_20_034509) do
+ActiveRecord::Schema.define(version: 2019_02_20_044947) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 2019_02_20_034509) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "illust_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["illust_id"], name: "index_comments_on_illust_id"
   end
 
   create_table "illusts", force: :cascade do |t|
