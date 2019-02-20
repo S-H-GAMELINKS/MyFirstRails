e5ab13db943c8f341b82bad253dde8f09ce5952b
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-20 17:28:52 +0900

    Migrate DB #72

--- mastodon_login/db/schema.rb
+++ mastodon_login/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_20_082700) do
+ActiveRecord::Schema.define(version: 2019_02_20_082829) do
+
+  create_table "mastodon_clients", force: :cascade do |t|
+    t.string "domain"
+    t.string "client_id"
+    t.string "client_secret"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
