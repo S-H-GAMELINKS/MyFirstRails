300c8a32eb73b517831aa2ad06ab2510f82fcb88
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-20 13:51:57 +0900

    add comment route #54

--- illust_post/config/routes.rb
+++ illust_post/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'illusts#index'
-  resources :illusts
+  resources :illusts do
+    resources :comments, :only => [:create, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
