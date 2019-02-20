80f979117bce89a8f6e4c735e83abcf4a79f0929
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-20 17:33:43 +0900

    add callback route #72

--- mastodon_login/config/routes.rb
+++ mastodon_login/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
