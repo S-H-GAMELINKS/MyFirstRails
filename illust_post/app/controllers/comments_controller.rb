28a3ab60d4377af4da627a8b8a2cc94c1ec39f84
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-20 13:55:12 +0900

    add comments_controller.rb #54

---
+++ illust_post/app/controllers/comments_controller.rb
@@ -0,0 +1,22 @@
+class CommentsController < ApplicationController
+    before_action :set_illust
+
+    def create
+        @illust.comments.create! comments_params
+        redirect_to @illust
+    end
+
+    def destroy
+        @illust.comments.destroy params[:id]
+        redirect_to @illust
+    end
+
+     private
+        def set_illust
+            @post = Illust.find(params[:illust_id])
+        end
+
+         def comments_params
+            params.required(:comment).permit(:content)
+        end
+end
