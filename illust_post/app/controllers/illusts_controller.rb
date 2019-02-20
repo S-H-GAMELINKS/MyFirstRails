981765f34c9c00de7bd13e1b8a07852bb0ebbfd9
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-20 14:49:00 +0900

    add check_login method #54

--- illust_post/app/controllers/illusts_controller.rb
+++ illust_post/app/controllers/illusts_controller.rb
@@ -1,5 +1,6 @@
 class IllustsController < ApplicationController
   before_action :set_illust, only: [:show, :edit, :update, :destroy]
+  before_action :check_login, only: [:new, :edit, :create, :update, :destroy]
 
   # GET /illusts
   # GET /illusts.json
@@ -77,6 +78,10 @@ class IllustsController < ApplicationController
           illust.purge
         end
       end
+    end
+
+    def check_login
+      redirect_to :root if current_user == nil
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
