9c753d92c6f343316b986f0cc473e45b2419fb16
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-20 17:28:11 +0900

    Migrate DB #72

--- mastodon_login/db/schema.rb
+++ mastodon_login/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_20_081755) do
+ActiveRecord::Schema.define(version: 2019_02_20_082700) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_02_20_081755) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uid"
+    t.string "provider"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
