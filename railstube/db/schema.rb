60a7cb47fe9f428b10505cd4a765431a620d7fa2
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-21 10:30:53 +0900

    Migrtae DB #71

--- railstube/db/schema.rb
+++ railstube/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_20_121700) do
+ActiveRecord::Schema.define(version: 2019_02_21_013032) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_02_20_121700) do
     t.integer "movie_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "score"
     t.index ["movie_id"], name: "index_comments_on_movie_id"
   end
 
