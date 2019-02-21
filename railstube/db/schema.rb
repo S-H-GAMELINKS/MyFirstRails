a23b0465531c425ea25ab01e4e88f4a826b2c951
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-21 12:27:01 +0900

    Migrate DB #71

--- railstube/db/schema.rb
+++ railstube/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_21_031652) do
+ActiveRecord::Schema.define(version: 2019_02_21_032622) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2019_02_21_031652) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
