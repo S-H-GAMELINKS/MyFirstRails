26b5b4e1f22867f42e393a1d5666f8e296fe370d
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-22 16:37:00 +0900

    add routes for products #31

--- qrpay/config/routes.rb
+++ qrpay/config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   root 'web#index'
   get '/about', to: 'web#index'
   get '/category', to: 'web#index'
+
+  get "/products", to: "web#index"
+  get "/products/:id", to: "web#index"
+  get "/products/:id/edit", to: "web#index"
+  get "/products/new", to: "web#index"
+
   namespace :api, format: 'json' do
     resources :products
   end
