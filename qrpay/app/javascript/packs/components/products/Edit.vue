a4519a60b90d88c187dfd87eea2b5e08f1f44753
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-22 16:34:21 +0900

    add Edit.vue for products #31

---
+++ qrpay/app/javascript/packs/components/products/Edit.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <product-form></product-form>
+    </div>
+</template>
+
+<script>
+import Form from './Form.vue';
+
+export default {
+    components: {
+        'product-form': Form
+    },
+}
+</script>
