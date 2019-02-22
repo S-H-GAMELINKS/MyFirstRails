7e61b7ae6dc5da909beb729c28ffb9e1002e87f3
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-22 16:14:06 +0900

    fix routes.rb #31

--- qrpay/config/routes.rb
+++ qrpay/config/routes.rb
@@ -1,7 +1,8 @@
-Rails.application.routes.draw do
-  namespace :api do
+Rails.application.routes.draw do  
+  root 'web#index'
+
+  namespace :api, format: 'json' do
     resources :products
   end
-  root 'web#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
