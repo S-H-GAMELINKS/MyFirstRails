14e08dc471af8ce4f24f668156932ce3eb45d82d
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-22 16:36:28 +0900

    import products crud #31

--- qrpay/app/javascript/packs/router/router.js
+++ qrpay/app/javascript/packs/router/router.js
@@ -4,6 +4,11 @@ import Index from '../components/web/Index.vue';
 import About from '../components/web/About.vue';
 import Contact from '../components/web/Contact.vue';
 
+import ProductsIndex from '../components/product/Index.vue';
+import ProductsCreate from '../components/product/Create.vue';
+import ProductsShow from '../components/product/Show.vue';
+import ProductsEdit from '../components/product/Edit.vue';
+
 Vue.use(VueRouter)
 
 export default new VueRouter({
@@ -12,5 +17,9 @@ export default new VueRouter({
     { path: '/', component: Index },
     { path: '/about', component: About },
     { path: '/contact', component: Contact },
+    { path: '/products', component: ProductsIndex },
+    { path: '/products/new', component: ProductsCreate },
+    { path: '/products/:id', component: ProductsShow, name: 'products_show'},
+    { path: '/products/:id/edit', component: ProductsEdit, name: 'products_edits'},
   ],
 })
