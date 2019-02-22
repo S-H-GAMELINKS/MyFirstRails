b52c68daf50cbd4474948ef1a360a0f8b16ec535
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-22 16:22:43 +0900

    add /about & /contact link #31

--- qrpay/app/javascript/packs/components/layouts/Header.vue
+++ qrpay/app/javascript/packs/components/layouts/Header.vue
@@ -1,13 +1,15 @@
 <template>
     <div>
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-            <a class="navbar-brand" href="/">Rails Pay</a>
+            <router-link class="navbar-brand" to="/">Rails Pay</router-link>
             <div class="dropdown">
                 <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Menu
                 </button>
                 <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a href="/" class="dropdown-item">Top</a>
+                    <router-link to="/" class="dropdown-item">Top</router-link>
+                    <router-link to="/about" class="dropdown-item">About</router-link>
+                    <router-link to="/category" class="dropdown-item">Category</router-link>
                 </div>
             </div>
         </nav>
