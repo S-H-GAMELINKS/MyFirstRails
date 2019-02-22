0c1c6759e229ac5cb1ab7edf677b6ce4c135044d
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-22 15:57:36 +0900

    load css for webpacker #31

--- qrpay/config/webpack/environment.js
+++ qrpay/config/webpack/environment.js
@@ -1,5 +1,13 @@
 const { environment } = require('@rails/webpacker')
 const vue =  require('./loaders/vue')
 
+environment.loaders.append('css', {
+    test: /\.css$/,
+    use: [
+        'style-loader',
+        'css-loader'
+    ]
+})
+
 environment.loaders.append('vue', vue)
 module.exports = environment
