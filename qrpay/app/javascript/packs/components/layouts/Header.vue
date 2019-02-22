1d051b60d9854f561e2d518e7692e05dad77c944
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-22 16:37:20 +0900

    add link to products #31

--- qrpay/app/javascript/packs/components/layouts/Header.vue
+++ qrpay/app/javascript/packs/components/layouts/Header.vue
@@ -10,6 +10,7 @@
                     <router-link to="/" class="dropdown-item">Top</router-link>
                     <router-link to="/about" class="dropdown-item">About</router-link>
                     <router-link to="/contact" class="dropdown-item">Contact</router-link>
+                    <router-link to="/products" class="dropdown-item">Product</router-link>
                 </div>
             </div>
         </nav>
