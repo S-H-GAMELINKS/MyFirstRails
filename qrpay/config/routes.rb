2eddc0ea16fa6024d30b183aa03ddee5511c068c
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-22 17:18:28 +0900

    add routes for payment #31

--- qrpay/config/routes.rb
+++ qrpay/config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   namespace :api, format: 'json' do
     resources :products
+    post '/payments' => 'payments#create'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
