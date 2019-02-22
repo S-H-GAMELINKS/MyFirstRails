07ac1ebb778815d4c384563c889b3c62c1dfd2c7
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-22 16:38:13 +0900

    fix #31

--- qrpay/app/javascript/packs/router/router.js
+++ qrpay/app/javascript/packs/router/router.js
@@ -4,10 +4,10 @@ import Index from '../components/web/Index.vue';
 import About from '../components/web/About.vue';
 import Contact from '../components/web/Contact.vue';
 
-import ProductsIndex from '../components/product/Index.vue';
-import ProductsCreate from '../components/product/Create.vue';
-import ProductsShow from '../components/product/Show.vue';
-import ProductsEdit from '../components/product/Edit.vue';
+import ProductsIndex from '../components/products/Index.vue';
+import ProductsCreate from '../components/products/Create.vue';
+import ProductsShow from '../components/products/Show.vue';
+import ProductsEdit from '../components/products/Edit.vue';
 
 Vue.use(VueRouter)
 
