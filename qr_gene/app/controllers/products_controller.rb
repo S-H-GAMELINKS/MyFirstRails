eebba8768aeec3f598ba81477a450a4193e85d69
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-25 11:36:25 +0900

    generate QR code for price #79

--- qr_gene/app/controllers/products_controller.rb
+++ qr_gene/app/controllers/products_controller.rb
@@ -10,6 +10,7 @@ class ProductsController < ApplicationController
   # GET /products/1
   # GET /products/1.json
   def show
+    @qr_code = QRCode::QRCode.new(@product.price.to_s).as_svg
   end
 
   # GET /products/new
