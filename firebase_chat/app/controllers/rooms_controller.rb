955a07614f9dea9fd482e10c05077ac8a5009ad7
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-28 17:54:19 +0900

    using gon #70

--- firebase_chat/app/controllers/rooms_controller.rb
+++ firebase_chat/app/controllers/rooms_controller.rb
@@ -10,6 +10,12 @@ class RoomsController < ApplicationController
   # GET /rooms/1
   # GET /rooms/1.json
   def show
+    gon.api_key = ENV['API_KEY']
+    gon.auth_domain = ENV['AUTH_DOMAIN']
+    gon.database_url = ENV['DB_URL']
+    gon.project_id = ENV['PROJECT_ID']
+    gon.storage_bucket = ENV['STORAGE_BUCKET']
+    gon.message_senderid = ENV['MESSAGEING_SENDER_ID']
   end
 
   # GET /rooms/new
