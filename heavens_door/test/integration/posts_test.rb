24e236a7aed4c2e1b66cbeae4d35bcf4c7f6cc6d
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-03-01 21:04:16 +0900

    add scenario #88

--- heavens_door/test/integration/posts_test.rb
+++ heavens_door/test/integration/posts_test.rb
@@ -6,5 +6,16 @@ class ActionDispatch::IntegrationTest
   include Capybara::Minitest::Assertions
 
   def heavens_door
+    scenario 'GENERATED' do
+        visit '/posts'
+    
+        click_link 'New Post'
+    
+        fill_in 'Title', with: 'test'
+        fill_in 'Content', with: 'aaaaaaaaaaaaaaaaaaaaaaaaa'
+        click_button 'Create Post'
+    
+        click_link 'Back'
+    end
   end
 end
