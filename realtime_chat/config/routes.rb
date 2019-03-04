d64b2861ca81901624a3b5ea24b7439e7a790e4d
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-03-04 13:44:22 +0900

    fix routes.rb #84

--- realtime_chat/config/routes.rb
+++ realtime_chat/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :rooms
+  root 'rooms#index'
+  resources :rooms do
+    resources :talks, :only => [:index, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
