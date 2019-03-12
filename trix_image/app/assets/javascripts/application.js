ac8bcb660a5a5024e41872cc69d97520bd2979cb
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-03-12 09:36:13 +0900

    add upload script #109

--- trix_image/app/assets/javascripts/application.js
+++ trix_image/app/assets/javascripts/application.js
@@ -15,3 +15,45 @@
 //= require trix
 //= require turbolinks
 //= require_tree .
+
+Trix.config.attachments.preview.caption = {
+    name: false,
+    size: false
+  };
+  
+   function uploadAttachment(attachment) {
+
+    var file = attachment.file;
+    var form = new FormData;
+    form.append("Content-Type", file.type);
+    form.append("photo[image]", file);
+  
+    var xhr = new XMLHttpRequest;
+    xhr.open("POST", "/photos.json", true);
+    xhr.setRequestHeader("X-CSRF-Token", Rails.csrfToken());
+  
+    xhr.upload.onprogress = function(event) {
+      var progress = event.loaded / event.total * 100;
+      attachment.setUploadProgress(progress);
+    }
+  
+    xhr.onload = function() {
+      if (xhr.status === 201) {
+        var data = JSON.parse(xhr.responseText);
+        return attachment.setAttributes({
+          url: data.image_url,
+          href: data.url
+        })
+      }
+    }
+  
+     return xhr.send(form);
+  }
+  
+   // Listen for the Trix attachment event to trigger upload
+  document.addEventListener("trix-attachment-add", function(event) {
+    var attachment = event.attachment;
+    if (attachment.file) {
+      return uploadAttachment(attachment);
+    }
+  });
