21e4cfd62dca5fe1d40c7209c8204b4c6b9c6f3f
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-03-12 09:28:32 +0900

    add shrine.rb #109

---
+++ trix_image/config/initializers/shrine.rb
@@ -0,0 +1,10 @@
+require "shrine"
+require "shrine/storage/file_system"
+
+ Shrine.storages = {
+  cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"),
+  store: Shrine::Storage::FileSystem.new("public", prefix: "uploads/store"),
+}
+
+ Shrine.plugin :activerecord
+Shrine.plugin :cached_attachment_data
