f71b6ee5b1b4c6f8e68d70e7f0478fbbb398bdca
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-03-19 11:32:10 +0900

    Migrate DB #115

--- papertrail/db/schema.rb
+++ papertrail/db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_19_021605) do
+ActiveRecord::Schema.define(version: 2019_03_19_022831) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "versions", force: :cascade do |t|
+    t.string "item_type", null: false
+    t.integer "item_id", null: false
+    t.string "event", null: false
+    t.string "whodunnit"
+    t.text "object", limit: 1073741823
+    t.datetime "created_at"
+    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
 end
