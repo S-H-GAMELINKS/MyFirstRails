886d77b97e65062a83f3ef5d7788947195213484
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-03-22 17:11:07 +0900

    using locale ja #134

--- railspress/config/application.rb
+++ railspress/config/application.rb
@@ -11,6 +11,8 @@ module Railspress
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.i18n.default_locale = :ja
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
