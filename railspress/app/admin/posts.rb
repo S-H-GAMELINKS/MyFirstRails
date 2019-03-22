1466f1a5edcdc7928398d7f6f60a5f5ebb3ce3c0
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-03-22 17:45:10 +0900

    fix post create #134

--- railspress/app/admin/posts.rb
+++ railspress/app/admin/posts.rb
@@ -1,15 +1,13 @@
 ActiveAdmin.register Post do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+    permit_params :title, :content, :auther, :date
+    form do |f|
+        f.inputs 'Article' do
+          f.input :title
+          f.input :content, as: :froala_editor
+          f.input :auther
+          f.input :date
+        end
+        f.actions
+      end
 
 end
