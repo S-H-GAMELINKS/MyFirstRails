297a137f6db2bfd26be24a5b7948c10651c1917c
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-03-25 16:49:36 +0900

    Create Post #134

---
+++ railspress/test/controllers/posts_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class PostsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get posts_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get posts_show_url
+    assert_response :success
+  end
+
+end
