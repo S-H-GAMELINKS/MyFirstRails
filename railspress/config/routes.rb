f015f5f558178c45944c4d8ff90a90184f4be81d
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-03-25 19:22:47 +0900

    add comments resources #134

--- railspress/config/routes.rb
+++ railspress/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'posts#index'
-  resources :posts, :only => [:index, :show]
+  resources :posts, :only => [:index, :show] do
+    resources :comments, :only => [:create, :destroy]
+  end
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
