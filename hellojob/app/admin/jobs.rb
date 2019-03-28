50112594f238d2c54f297100c030ea6b94efb646
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-03-28 15:06:46 +0900

    using editor #138

--- hellojob/app/admin/jobs.rb
+++ hellojob/app/admin/jobs.rb
@@ -1,15 +1,11 @@
 ActiveAdmin.register Job do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+    permit_params :title, :content
 
+    form do |f|
+        f.inputs 'Job' do
+            f.input :title
+            f.input :content, as:  :medium_editor, input_html: { data: { options: '{"spellcheck":false,"toolbar":{"buttons":["bold","italic","underline","anchor"]}}' } }
+        end
+        f.actions
+    end
 end
