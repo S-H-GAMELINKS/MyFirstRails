f5ea8fcc2794999a99ea73b308699c07c0797a98
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-03-28 14:43:32 +0900

    Migrate DB #138

--- hellojob/db/schema.rb
+++ hellojob/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_28_052729) do
+ActiveRecord::Schema.define(version: 2019_03_28_054059) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 2019_03_28_052729) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
