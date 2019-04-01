6153373f2912e2fd37a1b1eaa225ba99f7dc9fa5
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-01 17:25:50 +0900

    fix routes #138

--- hellojob/config/routes.rb
+++ hellojob/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'jobs/index'
-  get 'jobs/show'
+  resources :jobs, :only => [:index, :show]
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
