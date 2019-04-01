93d142770d9bbdb22409504ed817e48bdba6c05f
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-01 17:08:53 +0900

    Create Jobs#index & Jobs#show

--- hellojob/config/routes.rb
+++ hellojob/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'jobs/index'
+  get 'jobs/show'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
